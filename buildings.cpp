63f463066f2420d7cc0eee4f0478fcf1d1c98e83
ghazalrafiei <gh.rafiei99@gmail.com>
2022-04-14 12:45:25 +0430

    Clean

--- buildings.cpp
+++ buildings.cpp
@@ -1,11 +1,21 @@
-// g++ buildings.cpp -lglut -lGLU -lGL; ./a.out
 #include <GL/glut.h>
 #include <iostream>
 
+const int X_SHAPES_NO = 3; // 2*x-1 shapes at all
+const int Y_SHAPES_NO = 2;
+
+const float X_SIDE = 0.025;
+const float Y_SIDE = 0.05;
+
+const bool BLACK_THEME = false;
+
 void draw_grid() {
   glBegin(GL_LINES);
-  glColor3f(0.2f, 0.2f, 0.2f);
-  for (float i = -1; i < 1; i += 0.05) {
+  if (BLACK_THEME)
+    glColor3f(0.2f, 0.2f, 0.2f);
+  else
+    glColor3f(0.8f, 0.8f, 0.8f);
+  for (float i = -100 * Y_SIDE; i < 1; i += Y_SIDE) {
     glVertex2f(-1.f, i);
     glVertex2f(1.f, i);
 
@@ -15,35 +25,36 @@ void draw_grid() {
   glEnd();
 }
 
-void draw_square(float x, float y, float x_side, float y_side) {
+void draw_square(float x, float y, float X_SIDE, float Y_SIDE) {
   glBegin(GL_LINE_LOOP);
 
-  glColor3f(1, 1, 1);
+  if (BLACK_THEME)
+    glColor3f(1, 1, 1);
+  else
+    glColor3f(0, 0, 0);
 
-  glVertex2f(x + x_side, y + y_side);
-  glVertex2f(x + x_side, y - y_side);
-  glVertex2f(x - x_side, y - y_side);
-  glVertex2f(x - x_side, y + y_side);
+  glVertex2f(x + X_SIDE, y + Y_SIDE);
+  glVertex2f(x + X_SIDE, y - Y_SIDE);
+  glVertex2f(x - X_SIDE, y - Y_SIDE);
+  glVertex2f(x - X_SIDE, y + Y_SIDE);
   glEnd();
 }
 
 void draw_line(float x1, float y1, float x2, float y2) {
   glBegin(GL_LINES);
-  glColor3f(1, 1, 1);
+  if (BLACK_THEME)
+    glColor3f(1, 1, 1);
+  else
+    glColor3f(0, 0, 0);
   glVertex2f(x1, y1);
   glVertex2f(x2, y2);
   glEnd();
 }
 
 void draw_quarter(int x_q, int y_q) {
-  const float x_side = 0.05;
-  const float y_side = 0.05;
 
-  float x_margin = x_side * 4;
-  float y_margin = y_side * 4;
-
-  const int X_shape = 6; // 2*x-1 shapes at all
-  const int Y_shape = 5;
+  float x_margin = X_SIDE * 4;
+  float y_margin = Y_SIDE * 4;
 
   float x_axis = 0;
   float y_axis = 0;
@@ -51,41 +62,45 @@ void draw_quarter(int x_q, int y_q) {
   float x_axis_prime = 0;
   float y_axis_prime = 0;
 
-  for (int y = 1; y <= Y_shape; y++) {
-    for (int x = 1; x <= X_shape; x++) {
+  for (int y = 1; y <= Y_SHAPES_NO; y++) {
+    for (int x = 1; x <= X_SHAPES_NO; x++) {
       float dot1, dot2, dot3, dot4;
 
       // First square
-      draw_square(x_axis, y_axis, x_side, y_side);
+      draw_square(x_axis, y_axis, X_SIDE, Y_SIDE);
       dot1 = x_axis;
       dot2 = y_axis;
       x_axis = x_axis + x_q * x_margin;
-      x_margin += x_side;
+      x_margin += X_SIDE;
 
       // Second square
-      draw_square(x_axis_prime, y_axis_prime, x_side, y_side);
+      draw_square(x_axis_prime, y_axis_prime, X_SIDE, Y_SIDE);
       dot3 = x_axis_prime;
       dot4 = y_axis_prime;
-      x_axis_prime = x_axis + x_side * x * x_q;
+      x_axis_prime = x_axis + X_SIDE * x * x_q;
 
-      draw_line(dot1 - x_side, dot2 - y_side, dot3 - x_side, dot4 - y_side);
-      draw_line(dot1 - x_side, dot2 + y_side, dot3 - x_side, dot4 + y_side);
-      draw_line(dot1 + x_side, dot2 + y_side, dot3 + x_side, dot4 + y_side);
-      draw_line(dot1 + x_side, dot2 - y_side, dot3 + x_side, dot4 - y_side);
+      draw_line(dot1 - X_SIDE, dot2 - Y_SIDE, dot3 - X_SIDE, dot4 - Y_SIDE);
+      draw_line(dot1 - X_SIDE, dot2 + Y_SIDE, dot3 - X_SIDE, dot4 + Y_SIDE);
+      draw_line(dot1 + X_SIDE, dot2 + Y_SIDE, dot3 + X_SIDE, dot4 + Y_SIDE);
+      draw_line(dot1 + X_SIDE, dot2 - Y_SIDE, dot3 + X_SIDE, dot4 - Y_SIDE);
     }
 
     y_axis = y_axis + y_q * y_margin;
-    y_margin += y_side;
+    y_margin += Y_SIDE;
     x_axis = 0;
-    x_margin = x_side * 4;
+    x_margin = X_SIDE * 4;
 
-    y_axis_prime = y_axis + y_q * y_side * y;
+    y_axis_prime = y_axis + y_q * Y_SIDE * y;
     x_axis_prime = 0;
   }
 }
 
 void display() {
-  glClearColor(0.0, 0.0, 0.0, 0);
+  if (BLACK_THEME)
+    glClearColor(0.0, 0.0, 0.0, 0);
+  else
+    glClearColor(1.0, 1.0, 1.0, 0);
+
   glClear(GL_COLOR_BUFFER_BIT);
 
   draw_grid();
--- fun_pattern.cpp
+++ fun_pattern.cpp
@@ -1,11 +1,11 @@
-//g++ buildings.cpp -lglut -lGLU -lGL; ./a.out
-#include<GL/glut.h>
-#include<iostream>
+// g++ buildings.cpp -lglut -lGLU -lGL; ./a.out
+#include <GL/glut.h>
+#include <iostream>
 
-void draw_grid(){
+void draw_grid() {
   glBegin(GL_LINES);
   glColor3f(0.8f, 0.8f, 0.8f);
-  for (float i = -1; i < 1; i+=0.05) {
+  for (float i = -1; i < 1; i += 0.05) {
     glVertex2f(-1.f, i);
     glVertex2f(1.f, i);
 
@@ -15,93 +15,91 @@ void draw_grid(){
   glEnd();
 }
 
-
-void draw_square(float x, float y, float x_side, float y_side, bool color=false){
+void draw_square(float x, float y, float x_side, float y_side,
+                 bool color = false) {
   glBegin(GL_LINE_LOOP);
-    if(color)
-      glColor3f(0.0,0.0,0.0);
-    else
-      glColor3f(1, 1, 1);
+  if (color)
+    glColor3f(0.0, 0.0, 0.0);
+  else
+    glColor3f(1, 1, 1);
 
-    glVertex2f(x+x_side, y+y_side);
-    glVertex2f(x+x_side, y-y_side);
-    glVertex2f(x-x_side, y-y_side);
-    glVertex2f(x-x_side, y+y_side);
+  glVertex2f(x + x_side, y + y_side);
+  glVertex2f(x + x_side, y - y_side);
+  glVertex2f(x - x_side, y - y_side);
+  glVertex2f(x - x_side, y + y_side);
   glEnd();
 }
 
-
-void draw_line(float x1, float y1, float x2, float y2){
+void draw_line(float x1, float y1, float x2, float y2) {
   glBegin(GL_LINES);
-    glColor3f(0, 0, 0);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y2);
+  glColor3f(0, 0, 0);
+  glVertex2f(x1, y1);
+  glVertex2f(x2, y2);
   glEnd();
 }
 
-void draw_quarter(int x_q, int y_q){
-    const float x_side = 0.05;
-    const float y_side = 0.05;
+void draw_quarter(int x_q, int y_q) {
+  const float x_side = 0.05;
+  const float y_side = 0.05;
 
-    float x_margin = x_side*4;
-    float y_margin = y_side*4;
+  float x_margin = x_side * 4;
+  float y_margin = y_side * 4;
 
-    const int X_shape = 6; //2*x-1 shapes at all
-    const int Y_shape = 5;
+  const int X_shape = 6; // 2*x-1 shapes at all
+  const int Y_shape = 5;
 
-    float x_axis = 0;
-    float y_axis = 0;
+  float x_axis = 0;
+  float y_axis = 0;
 
-    float x_axis_prime = 0;
-    float y_axis_prime = 0;
+  float x_axis_prime = 0;
+  float y_axis_prime = 0;
 
-    
-    for (int y = 1; y<=Y_shape; y++){
-      for (int x = 1; x <= X_shape; x++) {
-        float dot1,dot2,dot3,dot4;
+  for (int y = 1; y <= Y_shape; y++) {
+    for (int x = 1; x <= X_shape; x++) {
+      float dot1, dot2, dot3, dot4;
 
-        // First square
-        draw_square(x_axis, y_axis, x_side, y_side);
-        dot1 = x_axis;
-        dot2 = y_axis;
-        x_axis = x_axis+ x_q*x_margin;
-        x_margin += x_side;
+      // First square
+      draw_square(x_axis, y_axis, x_side, y_side);
+      dot1 = x_axis;
+      dot2 = y_axis;
+      x_axis = x_axis + x_q * x_margin;
+      x_margin += x_side;
 
-        //Second square
-        draw_square(x_axis_prime, y_axis_prime, x_side, y_side, true);
-        dot3= x_axis_prime;
-        dot4= y_axis_prime;
-        x_axis_prime=x_axis+ x_side*x*x_q;
+      // Second square
+      draw_square(x_axis_prime, y_axis_prime, x_side, y_side, true);
+      dot3 = x_axis_prime;
+      dot4 = y_axis_prime;
+      x_axis_prime = x_axis + x_side * x * x_q;
 
-        draw_line(dot1-x_side,dot2-y_side,dot3-x_side,dot4-y_side);
-        draw_line(dot1-x_side,dot2+y_side,dot3-x_side,dot4+y_side);
-        draw_line(dot1+x_side,dot2+y_side,dot3+x_side,dot4+y_side);
-        draw_line(dot1+x_side,dot2-y_side,dot3+x_side,dot4-y_side);
-      }
-
-      y_axis = y_axis + y_q*y_margin;
-      y_margin+=y_side;
-      x_axis = 0;
-      x_margin = x_side*4;
-
-      y_axis_prime = y_axis+ y_q*y_side*y;
-      x_axis_prime = 0;
+      draw_line(dot1 - x_side, dot2 - y_side, dot3 - x_side, dot4 - y_side);
+      draw_line(dot1 - x_side, dot2 + y_side, dot3 - x_side, dot4 + y_side);
+      draw_line(dot1 + x_side, dot2 + y_side, dot3 + x_side, dot4 + y_side);
+      draw_line(dot1 + x_side, dot2 - y_side, dot3 + x_side, dot4 - y_side);
     }
+
+    y_axis = y_axis + y_q * y_margin;
+    y_margin += y_side;
+    x_axis = 0;
+    x_margin = x_side * 4;
+
+    y_axis_prime = y_axis + y_q * y_side * y;
+    x_axis_prime = 0;
+  }
 }
 
-void display(){
-    glClearColor(0.0, 0.0 , 0.0 , 0);
-    glClear(GL_COLOR_BUFFER_BIT);
+void display() {
+  glClearColor(0.0, 0.0, 0.0, 0);
+  glClear(GL_COLOR_BUFFER_BIT);
 
-    draw_grid();
+  draw_grid();
 
-    draw_quarter(1, 1);
-    draw_quarter(1, -1);
-    draw_quarter(-1, 1);
-    draw_quarter(-1, -1);
+  draw_quarter(1, 1);
+  draw_quarter(1, -1);
+  draw_quarter(-1, 1);
+  draw_quarter(-1, -1);
 
-  glPopMatrix(); 
-  glFlush();   
+  glPopMatrix();
+  glFlush();
 }
 
 int main(int argc, char **argv) {
@@ -109,7 +107,7 @@ int main(int argc, char **argv) {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(900, 900);
   glutInitWindowPosition(250, 90);
-  glutCreateWindow("A city from above");
+  glutCreateWindow("Fun pattern");
   glutDisplayFunc(display);
   glutMainLoop();
   return 0;
